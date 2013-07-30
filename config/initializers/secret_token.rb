81c8d7b3655500af62cf45fe05b21e56ac00c47d
Timothy S. Williams <t.williams.725@gmail.com>
2013-07-30 16:45:50 -0400

    Initialize Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WgArchive::Application.config.secret_key_base = '8375e197ad11e5b8a4139216d3cd97a1fd304b74cba1c973c4ee7ddc62f70726b5fdf2b617b28f53c658104412c2e5e9bf6a7799deaff89317cf3ffe1fda63a2'
